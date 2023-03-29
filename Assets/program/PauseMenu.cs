184e752235d57c4aa7a07eaf10faf1e5fef6e7e6
DESKTOP-SPD4CPI\ubin <gos920224@gmail.com>
2023-03-29 09:31:22 +0800

    新增遊戲內暫停(目前有小bug)、重新開始關卡、返回遊戲主選單的功能 目前的bug是1.按resume沒反應2.如果返回主菜單再次進入關卡實需要再按一次esc

---
+++ Assets/program/PauseMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}

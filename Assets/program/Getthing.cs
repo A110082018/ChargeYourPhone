416338c9530cc4bc16ed9b01a29f36df51c75d08
HAN <hanhan5556888@gmail.com>
2023-04-27 22:17:39 +0800

    加上endgamecontroller和UIcontroller的初始架構

---
+++ Assets/program/Getthing.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Getthing : MonoBehaviour
+{
+
+    public Image UI1;
+    public Image UI2;
+    public Image UI3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI1.enabled = false;
+        UI2.enabled = false;
+        UI3.enabled = false;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Littlegamefinal.finish ==true)
+        {
+            UI1.enabled = true;
+        }
+        if (Littlegame2final.finish2 == true)
+        {
+            UI2.enabled = true;
+        }
+
+    }
+
+
+}

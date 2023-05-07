29d1095a3a6057ccecc53f96116ecb6eefcacb4e
DESKTOP-SPD4CPI\ubin <gos920224@gmail.com>
2023-05-08 06:43:23 +0800

    新增進入指定空間內，跳出CANVAS提醒玩家周圍有道具的功能，優化場景內物件

---
+++ Assets/program/AreaTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AreaTrigger : MonoBehaviour
+{
+    public GameObject panel;
+    public float displayTime = 3.0f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // 當玩家進入區域時，顯示Panel
+            panel.SetActive(true);
+            // 在displayTime秒後，隱藏Panel
+            StartCoroutine(HidePanelAfterTime(displayTime));
+        }
+    }
+
+    IEnumerator HidePanelAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        panel.SetActive(false);
+    }
+}
+
